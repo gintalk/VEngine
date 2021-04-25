fe06fcb78ccfb6817e3a6e1e5f4e62bbcb4a361c
namnh16 <hnam9430@gmail.com>
2021-04-25 17:40:54 +0700

    Add ObservationDal

---
+++ src/com/vgu/cs/engine/dal/ObservationDal.java
@@ -0,0 +1,20 @@
+package com.vgu.cs.engine.dal;
+
+/*
+ * Copyright (c) 2012-2016 by Zalo Group.
+ * All Rights Reserved.
+ *
+ * @author namnh16 on 25/04/2021
+ */
+
+import com.vgu.cs.engine.dao.ObservationDao;
+import com.vgu.cs.engine.entity.ObservationEntity;
+
+public class ObservationDal extends PostgreSqlDal<ObservationEntity> {
+    
+    public static final ObservationDal INSTANCE = new ObservationDal();
+    
+    protected ObservationDal() {
+        super("observation", new ObservationDao("bestdb"));
+    }
+}
