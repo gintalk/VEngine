792c2cd2ce1c8b0f0ed11fda0ce39351f1c265e4
namnh16 <hnam9430@gmail.com>
2021-05-05 22:56:08 +0700

    Add ProcedureOccurrenceDal

---
+++ src/com/vgu/cs/engine/dal/ProcedureOccurrenceDal.java
@@ -0,0 +1,20 @@
+package com.vgu.cs.engine.dal;
+
+/*
+ * Copyright (c) 2012-2016 by Zalo Group.
+ * All Rights Reserved.
+ *
+ * @author namnh16 on 05/05/2021
+ */
+
+import com.vgu.cs.engine.dao.ProcedureOccurrenceDao;
+import com.vgu.cs.engine.entity.ProcedureOccurrenceEntity;
+
+public class ProcedureOccurrenceDal extends PostgreSqlDal<ProcedureOccurrenceEntity> {
+
+    public static final ProcedureOccurrenceDal INSTANCE = new ProcedureOccurrenceDal();
+
+    protected ProcedureOccurrenceDal() {
+        super("procedure_occurrence", new ProcedureOccurrenceDao("bestdb"));
+    }
+}
