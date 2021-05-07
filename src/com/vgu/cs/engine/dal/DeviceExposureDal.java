86572df195ad8d4b1b55766617c3a7b02899c332
namnh16 <hnam9430@gmail.com>
2021-05-08 00:43:26 +0700

    Add DeviceExposureDal

---
+++ src/com/vgu/cs/engine/dal/DeviceExposureDal.java
@@ -0,0 +1,20 @@
+package com.vgu.cs.engine.dal;
+
+/*
+ * Copyright (c) 2012-2016 by Zalo Group.
+ * All Rights Reserved.
+ *
+ * @author namnh16 on 08/05/2021
+ */
+
+import com.vgu.cs.engine.dao.DeviceExposureDao;
+import com.vgu.cs.engine.entity.DeviceExposureEntity;
+
+public class DeviceExposureDal extends PostgreSqlDal<DeviceExposureEntity> {
+
+    public static final DeviceExposureDal INSTANCE = new DeviceExposureDal();
+
+    protected DeviceExposureDal() {
+        super("device_exposure", new DeviceExposureDao("bestdb"));
+    }
+}
