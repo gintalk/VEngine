937feb33a26e75ea357ea71205eca6fa8dea94dd
namnh16 <hnam9430@gmail.com>
2021-05-08 01:19:11 +0700

    Add SpecimenDal

---
+++ src/com/vgu/cs/engine/dal/SpecimenDal.java
@@ -0,0 +1,20 @@
+package com.vgu.cs.engine.dal;
+
+/*
+ * Copyright (c) 2012-2016 by Zalo Group.
+ * All Rights Reserved.
+ *
+ * @author namnh16 on 08/05/2021
+ */
+
+import com.vgu.cs.engine.dao.SpecimenDao;
+import com.vgu.cs.engine.entity.SpecimenEntity;
+
+public class SpecimenDal extends PostgreSqlDal<SpecimenEntity> {
+
+    public static final SpecimenDal INSTANCE = new SpecimenDal();
+
+    protected SpecimenDal() {
+        super("specimen", new SpecimenDao("bestdb"));
+    }
+}
